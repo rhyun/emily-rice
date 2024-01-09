7a27ac4f92c42f870d16b5703ce8ba66e32ce311
Richard Hyun <rhyun@Richards-MacBook-Air.local>
2024-01-09 17:54:34 -0500

    Roots/Sage Setup & Config

--- bud.config.js
+++ bud.config.js
@@ -16,7 +16,7 @@ export default async (app) => {
   app
     .entry('app', ['@scripts/app', '@styles/app'])
     .entry('editor', ['@scripts/editor', '@styles/editor'])
-    .assets(['images']);
+    .assets(['images', 'fonts']);
 
   /**
    * Set public path
