544eaf5e4b873cef749a7a2f07e94a473823ca68
Richard Hyun <rhyun@Richards-MacBook-Air.local>
2024-02-01 03:01:54 -0500

    updated header

--- resources/views/sections/header.blade.php
+++ resources/views/sections/header.blade.php
@@ -1,51 +1,27 @@
-<header class="banner">
+<header class="banner px-4 py-2 md:p-4 lg:p-8 xl:p-12">
     {{-- @if (has_nav_menu('primary_navigation'))
         <nav class="nav-primary" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
             {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'echo' => false]) !!}
         </nav>
     @endif --}}
 
-    <div class="navbar bg-base-100">
+    <div class="navbar">
         <div class="navbar-start">
-            <div class="dropdown">
-                <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
-                        stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M4 6h16M4 12h16M4 18h7" />
-                    </svg>
-                </div>
-                <ul tabindex="0"
-                    class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                    <li><a>Homepage</a></li>
-                    <li><a>Portfolio</a></li>
-                    <li><a>About</a></li>
-                </ul>
-            </div>
+            <a class="btn text-white" href="/list-a-job" role="button">List a Job</a>
         </div>
         <div class="navbar-center">
-            <a class="brand" href="{{ home_url('/') }}">
+            <a class="brand block border-none" href="{{ home_url('/') }}">
+                @hasoption('branding', 'logo')
+                <img class="h-full max-h-12 w-full lg:max-h-16 xl:max-h-20" src="@option('branding', 'logo')
+"
+                    alt="{{ get_bloginfo('name', 'display') }}" />
+            @else
                 {!! $siteName !!}
-            </a>
-        </div>
-        <div class="navbar-end">
-            <button class="btn btn-ghost btn-circle">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
-                    stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                </svg>
-            </button>
-            <button class="btn btn-ghost btn-circle">
-                <div class="indicator">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
-                        stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
-                    </svg>
-                    <span class="badge badge-xs badge-primary indicator-item"></span>
-                </div>
-            </button>
-        </div>
+            @endoption
+        </a>
     </div>
+    <div class="navbar-end">
+        @include('components.contact')
+    </div>
+</div>
 </header>
